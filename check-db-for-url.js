4de0c6d31f155eeb9a79296fcbdf6b0eb32fc028
Bob Monsour <bob.monsour@gmail.com>
2024-06-05 07:37:04 -0700

    Tweaked messages on url check

--- check-db-for-url.js
+++ check-db-for-url.js
@@ -2,30 +2,30 @@ import inquirer from "inquirer";
 import chalk from "chalk";
 import { checkForDuplicateUrl } from "./utils.js";
 
-// Main function to prompt for a link to see if it's already in the db
+// Main function to prompt for a URL to see if it's already in the db
 const main = async () => {
   // console.log(chalk.red("Latest issue number:", getLatestIssueNumber()));
-  const { link } = await inquirer.prompt([
+  const { url } = await inquirer.prompt([
     {
       type: "input",
-      name: "link",
-      message: "Enter link to search for:",
+      name: "url",
+      message: "Enter URL to search for:",
     },
   ]);
 
-  // Check if the link is already in the database
-  const isDuplicate = checkForDuplicateUrl(link);
+  // Check if the url is already in the database
+  const isDuplicate = checkForDuplicateUrl(url);
   if (isDuplicate) {
-    console.log(chalk.red("Link is already in the database"));
+    console.log(chalk.red("URL is already in the database"));
   } else {
-    console.log(chalk.green("Link is not in the database"));
+    console.log(chalk.green("URL is not in the database"));
   }
 
   const restart = await inquirer.prompt([
     {
       type: "confirm",
       name: "restart",
-      message: "Do you want to check another link?",
+      message: "Do you want to check another URL?",
       default: false,
     },
   ]);
